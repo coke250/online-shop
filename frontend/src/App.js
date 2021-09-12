28739c67a8c4450c2c9bafbf4b24d3adaa464838
SSABI <fhy1930@gmail.com>
2021-09-12 22:17:07 +0900

    NotFound 코드 작성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Cart from './components/Cart';
 import Home from './components/Home';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
@@ -11,7 +12,9 @@ function App() {
         <NavBar />
         <Switch>
           <Route path="/cart" component={Cart} />
-          <Route path="/" component={Home} />
+          <Route path="/not-found" component={NotFound} />
+          <Route path="/" exact component={Home} />
+          <Redirect to="not-found" />
         </Switch>
       </BrowserRouter>
     </div>
