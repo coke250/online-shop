a863a414b40a1914efc483fb871cb2d71b7c3252
SSABI <fhy1930@gmail.com>
2021-09-12 22:52:47 +0900

    array of Products 생성

---
+++ backend/products.js
@@ -0,0 +1,25 @@
+const products = [
+    {
+        id: 1,
+        name: "iPhone 12 Pro",
+        desc: "6.1-inch display",
+        price: 999,
+        image: "https://res.cloudinary.com/dw8vzlv29/image/upload/v1631454584/onlineShop/iphone12pro_d0vsuk.jpg",
+    },
+    {
+        id: 2,
+        name: "iPhone 12",
+        desc: "5.4-inch display",
+        price: 699,
+        image: "https://res.cloudinary.com/dw8vzlv29/image/upload/v1631454583/onlineShop/iphone12_pcrjc2.jpg",
+    },
+    {
+        id: 3,
+        name: "Galaxy S",
+        desc: "6.5-inch display",
+        price: 399,
+        image: "https://res.cloudinary.com/dw8vzlv29/image/upload/v1631454583/onlineShop/galaxyS_iizjfb.png",
+    },
+];
+
+module.exports = products;
