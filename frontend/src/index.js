737d0f8c2f5d761731029f380f8ea065084a925d
SSABI <fhy1930@gmail.com>
2021-09-12 23:30:39 +0900

    productsSlice 코드 작성

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,9 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+
+import productsReducer from './features/productsSlice';
+
+const store = configureStore({
+  reducer: {productsReducer},
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
