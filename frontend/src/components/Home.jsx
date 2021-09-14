08adca9e969c0963cf8a438c06088d72a6c81f76
SSABI <fhy1930@gmail.com>
2021-09-14 21:59:13 +0900

    product 목록 출력

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,8 +1,34 @@
-import { useGetAllProductsQuery } from '../features/productsApi';
+import { productsApi, useGetAllProductsQuery } from '../features/productsApi';
 
 const Home = () => {
   const { data, error, isLoading } = useGetAllProductsQuery();
-  return <h2>Home</h2>;
+  return (
+    <div className="home-container">
+      {isLoading ? (
+        <p>loading...</p>
+      ) : error ? (
+        <p>An error occured...</p>
+      ) : (
+        <>
+        <h2>New Arrivals</h2>
+        <div className="products">
+          {data?.map(product => 
+            <div key={product.id} className="product">
+              <h3>{product.name}</h3>
+              <img src={product.image} alt={product.name} />
+              <div className="details">
+                <span>{product.desc}</span>
+                <span className="price">${product.price}</span>
+              </div>
+              <button>Add To Cart</button>
+            </div>
+
+            )}
+        </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Home;
