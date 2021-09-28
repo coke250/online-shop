4ee6923a681725b1d64033966389bc29a9cb8917
SSABI <fhy1930@gmail.com>
2021-09-28 21:53:19 +0900

    장바구니 증가/감소 기능 추가

--- frontend/src/components/Cart.jsx
+++ frontend/src/components/Cart.jsx
@@ -1,7 +1,7 @@
 import { useSelector, useDispatch } from 'react-redux';
 
 import { Link } from 'react-router-dom';
-import { removeFromCart } from '../features/cartSlice';
+import { addToCart, decreaseCart, removeFromCart } from '../features/cartSlice';
 
 const Cart = () => {
   const cart = useSelector((state) => state.cart);
@@ -9,7 +9,15 @@ const Cart = () => {
 
   const handleRemoveFromCart = (cartItem) => {
     dispatch(removeFromCart(cartItem));
-  }
+  };
+
+  const handleDecreaseCart = (cartItem) => {
+    dispatch(decreaseCart(cartItem));
+  };
+
+  const handleIncreaseCart = (cartItem) => {
+    dispatch(addToCart(cartItem));
+  };
 
   return (
     <div className="cart-container">
@@ -52,14 +60,20 @@ const Cart = () => {
                   <div>
                     <h3>{cartItem.name}</h3>
                     <p>{cartItem.desc}</p>
-                    <button onClick={() => handleRemoveFromCart(cartItem)}>Remove</button>
+                    <button onClick={() => handleRemoveFromCart(cartItem)}>
+                      Remove
+                    </button>
                   </div>
                 </div>
                 <div className="cart-product-price">${cartItem.price}</div>
                 <div className="cart-product-quantity">
-                  <button>-</button>
+                  <button onClick={() => handleDecreaseCart(cartItem)}>
+                    -
+                  </button>
                   <div className="count">{cartItem.cartQuantity}</div>
-                  <button>+</button>
+                  <button onClick={() => handleIncreaseCart(cartItem)}>
+                    +
+                  </button>
                 </div>
                 <div className="cart-product-total-price">
                   ${cartItem.price * cartItem.cartQuantity}
